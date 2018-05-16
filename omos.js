915b0f1cc4eb19ef4b445f12a3a180166cdb2e91
mind0004 <mind0004@stud.kea.dk>
2018-05-16 11:46:55 +0200

    asd

--- omos.js
+++ omos.js
@@ -1,18 +1,28 @@
-/*document.addEventListener("DOMContentLoaded", hentJson);
+document.addEventListener("DOMContentLoaded", hentJson);
 
 async function hentJson() {
 
     var urlParams = new URLSearchParams(window.location.search);
-    let id = urlParams.get("kobhosscharling");
-    let jsonObjekt = await fetch("http://meifriis.com/kea/scharlingwoodwp/wp-json/wp/v2/kobhosscharling/13");
-    let kobhosscharling = await jsonObjekt.json();
+    let id = urlParams.get("omos");
+    let jsonObjekt = await fetch("http://meifriis.com/kea/scharlingwoodwp/wp-json/wp/v2/omos/42/");
+    let omos = await jsonObjekt.json();
 
-    document.querySelector("[data-overskrift]").innerHTML = kobhosscharling.acf.kobhosscharlingoverskrift;
-    document.querySelector("[data-telefon]").innerHTML = kobhosscharling.acf.telefon;
-    document.querySelector("[data-email]").innerHTML = kobhosscharling.acf.email;
-    document.querySelector("[data-tekst]").innerHTML = kobhosscharling.acf.tekst;
-    document.querySelector("[data-overskrift2]").innerHTML = kobhosscharling.acf.overskrift2;
-    document.querySelector("[data-varkstedtekst]").innerHTML = kobhosscharling.acf.varkstedtekst;
-    document.querySelector("[data-meretekst]").innerHTML = kobhosscharling.acf.meretekst;
+    document.querySelector("[data-overkskrift]").innerHTML = omos.acf.overkskrift;
+    document.querySelector("[data-img]").src = omos.acf.img.url;
 
-}*/
+    document.querySelector("[data-overskrift1]").innerHTML = omos.acf.overskrift1;
+    document.querySelector("[data-tekst1]").innerHTML = omos.acf.tekst1;
+
+    document.querySelector("[data-img2]").src = omos.acf.img2.url;
+    document.querySelector("[data-overskrift3]").innerHTML = omos.acf.overskrift3;
+    document.querySelector("[data-tekst3]").innerHTML = omos.acf.tekst3;
+
+    document.querySelector("[data-img4]").src = omos.acf.img4.url;
+    document.querySelector("[data-overskrift4]").innerHTML = omos.acf.overskrift4;
+    document.querySelector("[data-tekst4]").innerHTML = omos.acf.tekst4;
+
+    document.querySelector("[data-img5]").src = omos.acf.img5.url;
+    document.querySelector("[data-overskrift5]").innerHTML = omos.acf.overskrift5;
+    document.querySelector("[data-tekst5]").innerHTML = omos.acf.tekst5;
+
+}

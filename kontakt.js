353caad6b03473bec1ea7476b5a4d7147aaac41e
mind0004 <mind0004@stud.kea.dk>
2018-05-16 11:27:14 +0200

    asd

--- kontakt.js
+++ kontakt.js
@@ -3,16 +3,16 @@ document.addEventListener("DOMContentLoaded", hentJson);
 async function hentJson() {
 
     var urlParams = new URLSearchParams(window.location.search);
-    let id = urlParams.get("kobhosscharling");
-    let jsonObjekt = await fetch("http://meifriis.com/kea/scharlingwoodwp/wp-json/wp/v2/kobhosscharling/13");
-    let kobhosscharling = await jsonObjekt.json();
+    let id = urlParams.get("kontakt");
+    let jsonObjekt = await fetch("http://meifriis.com/kea/scharlingwoodwp/wp-json/wp/v2/kontakt/13");
+    let kontakt = await jsonObjekt.json();
 
-    document.querySelector("[data-overskrift]").innerHTML = kobhosscharling.acf.kobhosscharlingoverskrift;
-    document.querySelector("[data-telefon]").innerHTML = kobhosscharling.acf.telefon;
-    document.querySelector("[data-email]").innerHTML = kobhosscharling.acf.email;
-    document.querySelector("[data-tekst]").innerHTML = kobhosscharling.acf.tekst;
-    document.querySelector("[data-overskrift2]").innerHTML = kobhosscharling.acf.overskrift2;
-    document.querySelector("[data-varkstedtekst]").innerHTML = kobhosscharling.acf.varkstedtekst;
-    document.querySelector("[data-meretekst]").innerHTML = kobhosscharling.acf.meretekst;
+    document.querySelector("[data-overskrift]").innerHTML = kontakt.acf.kontaktoverskrift;
+    document.querySelector("[data-telefon]").innerHTML = kontakt.acf.telefon;
+    document.querySelector("[data-email]").innerHTML = kontakt.acf.email;
+    document.querySelector("[data-tekst]").innerHTML = kontakt.acf.tekst;
+    document.querySelector("[data-overskrift2]").innerHTML = kontakt.acf.overskrift2;
+    document.querySelector("[data-varkstedtekst]").innerHTML = kontakt.acf.varkstedtekst;
+    document.querySelector("[data-meretekst]").innerHTML = kontakt.acf.meretekst;
 
 }
